915cdae2167083770c530983aff84abd6afbcec0
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-11-22 09:09:38 +0100

    Exercise15 resuelto

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -3,6 +3,40 @@ package com.campusdual.classroom;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class FreshMerchandise extends Merchandise {
+public class FreshMerchandise extends Merchandise{
 
+    private Date experationDate;
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area,
+                           String shelf, int quantity, Date experationDate){
+    super(name, uniqueId,responsibleId, zone, area, shelf, quantity);
+    this.experationDate=experationDate;
 }
+    @Override
+    public String getSpecificData(){
+        String date = getFormattedDate(this.experationDate);
+
+        return "Name: " + this.name +
+                ", Unique ID: " + this.uniqueId +
+                ", Responsible ID: " + this.responsibleId +
+                ", Zone: " + this.zone +
+                ", Area: " + this.area +
+                ", Shelf: " + this.shelf +
+                ", Quantity: " + this. quantity +
+                ", Date: " + date;
+
+    }
+
+    public void printSpecificData(){
+        System.out.println(getSpecificData());
+    }
+
+    public String getFormattedDate(Date date){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(date);
+    }
+
+    public Date getExpirationDate(){
+        return this.experationDate;
+    }
+}
+
